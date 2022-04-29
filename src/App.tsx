831e27a3a3026c6fe01d04b2478b65988476bc0e
aplv <o.pyliavets@gmail.com>
2022-04-29 21:26:46 +0300

    Add h1 tag :wq

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 
+const Header = () => 
+  <h1>
+      Oleksandr Pyliavets
+  </h1>
+
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
     </div>
   );
 }
